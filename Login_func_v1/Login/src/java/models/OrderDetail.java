613a456c6d3e032580310f2dc9f808d63f8ed4c5
quanghieeu <quanghieuhg2005@gmail.com>
2025-10-28 02:02:54 +0700

    Add OrderDetail.java file (was missing after rename)

---
+++ Login_func_v1/Login/src/java/models/OrderDetail.java
@@ -0,0 +1,78 @@
+﻿package models;
+
+public class OrderDetail {
+    private int orderDetailID;
+    private int orderID;
+    private int productID;
+    private int quantity;
+    private double totalPrice;
+    private String specialInstructions;
+
+    public OrderDetail(int orderDetailID, int orderID, int productID, int quantity, double totalPrice, String specialInstructions) {
+        this.orderDetailID = orderDetailID;
+        this.orderID = orderID;
+        this.productID = productID;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+        this.specialInstructions = specialInstructions;
+    }
+
+    public OrderDetail() {
+    }
+
+    public int getOrderDetailID() {
+        return orderDetailID;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public String getSpecialInstructions() {
+        return specialInstructions;
+    }
+
+    public void setOrderDetailID(int orderDetailID) {
+        this.orderDetailID = orderDetailID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void setSpecialInstructions(String specialInstructions) {
+        this.specialInstructions = specialInstructions;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" + "orderDetailID=" + orderDetailID + ", orderID=" + orderID + ", productID=" + productID + ", quantity=" + quantity + ", totalPrice=" + totalPrice + ", specialInstructions=" + specialInstructions + '}';
+    }
+
+    
+    
+}
