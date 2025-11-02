654fe9ea5c498b1e7c6299360edd97fec999b0ad
quanghieeu <quanghieuhg2005@gmail.com>
2025-11-02 17:16:47 +0700

    Fix import: Orderdetail -> OrderDetail in AddOrderServlet

--- Login_func_v1/Login/src/java/controller/AddOrderServlet.java
+++ Login_func_v1/Login/src/java/controller/AddOrderServlet.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import models.Order;
-import models.Orderdetail;
+import models.OrderDetail;
 import models.Product;
 import models.User;
 
