357ea382d34b17fdbf9ddd4096b097112e0ada42
quanghieeu <quanghieuhg2005@gmail.com>
2025-11-15 12:17:01 +0700

    fixx_UI
    fixx

--- Login_func_v1/Login/src/java/dao/DBContext.java
+++ Login_func_v1/Login/src/java/dao/DBContext.java
@@ -7,8 +7,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DBContext {
-    private static final String USER = "thongqt";
-    private static final String PASSWORD = "123456";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "123";
     private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=pizza_demo_DB_FinalModel_Combined_6;encrypt=true;trustServerCertificate=true";
 
     // Load the SQL Server JDBC driver
